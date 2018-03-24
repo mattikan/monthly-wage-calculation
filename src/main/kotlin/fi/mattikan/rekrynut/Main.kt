c04f1c1c0cbb4fe04b6710659cc9fbef613f6616
Matti Känninen <mattikan@cs.helsinki.fi>
2018-03-24 19:41:27 +0200

    some initial work on wages

--- src/main/kotlin/fi/mattikan/rekrynut/Main.kt
+++ src/main/kotlin/fi/mattikan/rekrynut/Main.kt
@@ -1,13 +1,18 @@
 package main.kotlin.fi.mattikan.rekrynut
 
 import main.kotlin.fi.mattikan.rekrynut.io.*
+import main.kotlin.fi.mattikan.rekrynut.logic.Workday
 import java.io.File
+import java.time.LocalDate
 
 fun main(args: Array<String>) {
-    print("annapa pathi tiedostoon: ")
-    val hourListFile = File(readLine())
-    val hourList = readHourList(hourListFile)
+    //print("annapa pathi tiedostoon: ")
+    //val hourListFile = File(readLine())
+    //val hourList = readFile(hourListFile)
 
+    var testday = Workday(LocalDate.now(), 4.25, 4.50)
+    testday.addHours(4.25, 4.50)
+    println(testday.totalWage())
 }
 
 fun parseFile() {
--- src/main/kotlin/fi/mattikan/rekrynut/logic/Workday.kt
+++ src/main/kotlin/fi/mattikan/rekrynut/logic/Workday.kt
@@ -2,6 +2,21 @@ package main.kotlin.fi.mattikan.rekrynut.logic
 
 import java.time.LocalDate
 
-class Workday (val date: LocalDate ) {
+class Workday (val date: LocalDate, var regularHours: Double, var eveningHours: Double) {
 
+    fun addHours(hours: Double, eveningHours: Double) {
+        this.regularHours += hours
+        this.eveningHours += eveningHours
+    }
+
+    fun overtime(): Double {
+        // todo: calculate overtime compensation
+        return 0.00
+    }
+
+    fun totalWage(): Double {
+        // todo: regular + evening + overtime
+        return ((this.regularHours + this.eveningHours)*4.25 + this.eveningHours*1.25) //todo: plus overtime
+    }
 }
+
